f0c2edd1470a7210374e9ca86c860a80c41b11b6
zubquzaini <zubliquzaini@gmail.com>
2020-10-06 10:58:02 +0800

    Final countdown

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
 
+  @ViewChild('slides') slides: IonSlides;
+  
   constructor() {}
 
+  ionViewWillLeave(){
+    this.slides.stopAutoplay();
+  }
+
+  ionViewDidEnter() {
+    this.slides.startAutoplay();
+  }
+
+  slidesDidLoad(slides) {
+    slides.startAutoplay();
+  }
 }
